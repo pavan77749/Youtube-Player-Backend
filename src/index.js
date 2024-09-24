dd8e7817959d8091a9f399b01d2b31141c874f3b
Pavan Gupta <pavangpay05@gmail.com>
2024-09-24 23:48:59 +0530

    utils api error and success handling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
 
 dotenv.config()
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 ,() => {
+        console.log(`Server is running on PORT : ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("MONGODB connection failed !!", error)
+})
