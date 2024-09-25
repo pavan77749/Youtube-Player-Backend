41d88e65b777e66923cdbde05a41e93e6fb04493
Pavan Gupta <pavangpay05@gmail.com>
2024-09-25 20:34:21 +0530

    completed with setup of backend

--- src/app.js
+++ src/app.js
@@ -13,4 +13,13 @@ app.use(express.urlencoded({limit:"16kb",extended:true}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// routes
+
+import userRouter from "./routes/user.route.js"
+
+
+// routes declaration
+app.use("/api/v1/users",userRouter)
+// http://localhost:8000/api/v1/users/register
+
 export {app}
